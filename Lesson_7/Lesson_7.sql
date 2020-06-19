c19afc7d84a6fffc5fc478dc99c6529017994581
vah-michael <vm_vt@mail.ru>
2020-06-19 16:11:30 +0300

    edit home work Lesson_7.sql
    edit  task 1
    add execution task 2

--- Lesson_7/Lesson_7.sql
+++ Lesson_7/Lesson_7.sql
@@ -1,21 +1,24 @@
 -- задание 1
+------------ вариант 1
 select 
-id,
-name, 
-birthday_at
-(
-select count(*) as orders_count from orders group by user_id order by orders_count desc
-)
-from users where id in (select user_id from orders);
+	(select name from users where id = user_id) as name,
+	(select birthday_at from users where id = user_id) as birthday,
+	count(*) as orders_count
+from orders
+group by name
+order by orders_count desc;
 
-select name from users where id in (select user_id from orders);
-
-
-
-select user_id from orders;
-
-
+------------ вариант 2
+select name, birthday_at, count(*) as orders_count from 
+users u right join orders o
+	on u.id = o.user_id
+group by name
+order by orders_count desc;
 
 
+-- задание 2
+select p.name as products, c.name as catalogs from 
+products p join catalogs c
+	on p.id = c.id;
 
 
