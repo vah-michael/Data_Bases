5d61d5e285f58acae957fad86b578738085743d2
vah-michael <vm_vt@mail.ru>
2020-06-30 14:53:55 +0300

    add executed ex.1

--- Lesson_9/Lesson_9.sql
+++ Lesson_9/Lesson_9.sql
@@ -1,1 +1,16 @@
 -- задание 1
+use sample;
+
+
+delimiter //
+create procedure qqq (IN id_in int)
+    if not exists (select id from sample.users where id=id_in)
+     then INSERT INTO sample.users (id,name, birthday_at, created_at, updated_at) SELECT * FROM shop.users WHERE id= id_in;
+     end if;
+//
+
+start transaction;
+
+call qqq (1);
+
+commit;
